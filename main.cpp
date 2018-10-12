853bb2c27edc6f97b7ad3ac651386b166f79fc34
Zahar Tkachov <zaharchik49@gmail.com>
2018-10-12 19:50:22 +0300

    added more functions

--- main.cpp
+++ main.cpp
@@ -12,7 +12,22 @@
 using namespace std;
 using namespace crow;
 
-std::map<string, bool> used;
+struct car{
+    std :: string ID;
+    std :: string qual;
+};
+
+struct Orders{
+    std :: string name;
+    std :: string phone;
+    std :: vector <car> Cart;
+    bool used;
+};
+
+
+std :: map <string, bool> used;
+
+std :: map <std :: string, int> OrdUse;
 
 void start() {
     int rc = sqlite3_open("./restaurant.db", &db);
@@ -24,8 +39,21 @@ void start() {
     createTable("menu", {"id", "name", "price", "time", "description", "image"}, {"intkeynn", "stringnn", "stringnn", "stringnn", "stringnn", "stringnn"});
 }
 
+void start_order() {
+    int rc = sqlite3_open("./order.db", &db);
+    if (rc) {
+        cout << "Can't open order's data base!!!\n" << sqlite3_errmsg(db) << '\n';
+    } else {
+        cout << "Order's data base opened successfully!!!\n";
+    }
+    createTable("orders", {"id", "description"}, {"stringnn", "stringnn"});
+}
+
 std :: vector <std :: string> DishVec;
 std :: vector <std :: string> DishArray;
+
+std :: vector <Orders> OrderVec;
+std :: vector <std :: string> OrderArray;
 
 std :: string MinusSpace(std :: string s){
     while (s[0] == ' '){
@@ -48,14 +76,133 @@ void Convert_to_json(std :: string dish0, std :: string dish, std :: string pric
     used[rez] = true;
 }
 
-
-
-
 std :: string Convert_to_json_write(std :: string dish0, std :: string dish, std :: string price0, std :: string price, std :: string image0, std :: string image, std :: string description0, std :: string description, std :: string time0, std :: string time, std :: string ID0, std :: string ID){
     std :: string rez = "{ \"" + dish0 + "\":\"" + dish + "\", \"" + price0 + "\":\"" + price + "\", \"" + image0 + "\":\"" + image  + "\", \"" + description0 + "\":\"" + description + "\", \"" + time0 + "\":\"" + time + "\", \"" + ID0 + "\":\"" + ID + "\" }" ;
     std :: cout << "\n" << rez;
     return rez;
 }
+
+//%%%%%%%%%%%%%%%%%%%%
+
+void Order_Processing_json(std :: string name0, std :: string name, std :: string phone0, std :: string phone, std::vector<car> car0){
+    cout << "\nOrder_Processing_json:successfully\n";
+    std :: string rez = "";
+    rez += "{ \"" + name0 + "\":\"" + name + "\", \"" + phone0 + "\":\"" + phone + "\", \"order_description\":[ ";
+    for (int i = 0; i < car0.size(); i++){
+        if (i != car0.size() - 1){
+            rez += "{ \"ID\":\"" + car0[i].ID + "\", \"quantity\":\"" + car0[i].qual + "\" }, ";
+        } else {
+            rez += "{ \"ID\":\"" + car0[i].ID + "\", \"quantity\":\"" + car0[i].qual + "\" }";
+        }
+    }
+    rez += " ] }";
+    cout << "\nrez:" << rez << "\n";
+    OrderArray.push_back(rez);
+}
+
+void Order_Processing_comand(std :: string s){
+    cout << "\nOrder_Processing_comand: successfully\n";
+    cout << "\nOPc:" << s << "\n";
+    s+= "_";
+    Orders ord;
+    ord.name = "";
+    ord.phone = "";
+    ord.Cart = { };
+    std :: string car1 = "";
+    std :: string car2 = "";
+    int top = 0;
+    int front = 0;
+    std :: string resault = "";
+    for (int i = 0; i < s.length(); i++){
+        if (s[i] != '_'){
+            if (s[i] == '-'){
+                s[i] = ' ';
+            }
+        } else {
+            top++;
+            if (top == 1){
+                ord.name = s.substr(front, i - front);
+            }
+            if (top == 2){
+                ord.phone = s.substr(front, i - front);
+            }
+            if (top >= 3 && top % 2 == 1){
+                car1 = "";
+                // ord.Cart.push_back(s.substr(front, i - front));
+                car1 += s.substr(front, i - front);
+                resault += car1 + "_";
+            }
+            if (top >= 3 && top % 2 == 0){
+                car2 = "";
+                car2 += s.substr(front, i - front);
+                resault += car2 + "_";
+                ord.Cart.push_back({car1, car2});
+            }
+            front = i + 1;
+        }
+        
+    }
+    cout << "\n" << ord.name + ord.phone + car1 + car2 << "\n";
+//
+
+//
+    OrdUse[ord.name + ord.phone + car1 + car2]++;
+    if (OrdUse[ord.name + ord.phone + car1 + car2] <= 1){
+        // OrderVec[ord.name + ord.phone + car1 + car2].used = true;
+        add("orders", {ord.name + ord.phone, resault});
+        Order_Processing_json("name", ord.name, "phone", ord.phone, ord.Cart);
+        OrderVec.push_back({ord.name, ord.phone, ord.Cart});
+    }
+    // if (!OrderVec[ord.name + ord.phone + car1 + car2].used){
+    //     OrderVec.push_back({ord.name, ord.phone, ord.Cart});
+    // }
+}
+
+
+std :: string ReturnOrderInfoJson(std :: string name0, std :: string name, std :: string phone0, std :: string phone, std::vector<car> car0){
+    cout << "\nOrder_Processing_json:successfully\n";
+    std :: string rez = "";
+    rez += "{ \"" + name0 + "\":\"" + name + "\", \"" + phone0 + "\":\"" + phone + "\", \"order_description\":[ ";
+    for (int i = 0; i < car0.size(); i++){
+        if (i != car0.size() - 1){
+            rez += "{ \"ID\":\"" + car0[i].ID + "\", \"quantity\":\"" + car0[i].qual + "\" }, ";
+        } else {
+            rez += "{ \"ID\":\"" + car0[i].ID + "\", \"quantity\":\"" + car0[i].qual + "\" }";
+        }
+    }
+    rez += " ] }";
+    cout << "\nrez:" << rez << "\n";
+    return rez;
+}
+
+
+std :: vector<car> ReturnOrderInfoComand(std :: string s){
+    s += "_";
+    int top = 0;
+    int front = 0;
+    std :: vector <car> res;
+    for (int i = 0; i < s.length(); i++){
+        if (s[i] != '_'){
+            if (s[i] == '-'){
+                s[i] = ' ';
+            }
+        } else {
+            top++;
+            if (top % 2 == 1){
+                res.push_back({});
+                res[res.size() - 1].ID = s.substr(front, i - front);
+            }
+            if (top % 2 == 0){
+                res[res.size() - 1].qual = s.substr(front, i - front);
+            }
+            front = i + 1;
+        }
+    }
+    return res;
+}
+
+
+//%%%%%%%%%%%%%%%%%%%%
 
 void Convert_to_comand(std :: string s){
     s += '_';
@@ -111,7 +258,7 @@ int main() {
 
     std :: string AddMeal = "";
     bool need;
-    
+    // start();
     
 
 
@@ -213,7 +360,9 @@ int main() {
     CROW_ROUTE(app, "/menu")([](const crow::request&, crow::response& res) -> void{
         res.add_header("Access-Control-Allow-Origin", "*");
 
-        // start();
+        start();
+
+
         // add("menu", {"1", "pizza", "23", "1", "tasty thing", "../image/png1.png"});
         // int ColAsk = 2;
         //dropTable("menu");
@@ -244,6 +393,103 @@ int main() {
         res.write(" ]");
         res.end();
     });
+
+    std :: string order_info = "";
+
+    CROW_ROUTE(app, "/add_order/<string>")
+        .methods("GET"_method, "POST"_method, "DELETE"_method)
+        ([&order_info](const request& req, const string& id) {
+            
+            if (req.method == "GET"_method)
+            {
+                // order_info = id;
+                cout << "\n\nid:" << id << "\n\n";
+                Order_Processing_comand(id);
+                return response(200, "You used GET");
+            }
+            else if (req.method == "POST"_method)
+            {
+                return response(200, "You used POST");
+            }
+            else if (req.method == "DELETE"_method)
+            {
+                return response(200, "You used DELETE");
+            }
+            else
+            {
+                return response(404);
+            }
+        });
+    // Order_Processing_comand(order_info);
+
+
+    CROW_ROUTE(app, "/order_menu")([](const crow::request&, crow::response& res) -> void{
+        res.add_header("Access-Control-Allow-Origin", "*");
+        start_order();
+
+        res.write("[ ");
+        cout << "\n\n\nOrderArray: " << OrderArray.size() << "\n\n\n";
+        for (int i = 0; i < OrderArray.size(); i++){
+            if (i != OrderArray.size() - 1){
+                res.write(OrderArray[i] + ", ");
+            } else {
+                res.write(OrderArray[i]);
+            }
+        }
+
+        res.write(" ]");
+        res.end();
+    });
+
+    CROW_ROUTE(app, "/info_order/<string>")
+        .methods("GET"_method, "POST"_method, "DELETE"_method)
+        ([&order_info](const request& req, const string& id) {
+            
+            if (req.method == "GET"_method)
+            {
+                order_info = id;
+                return response(200, "You used GET");
+            }
+            else if (req.method == "POST"_method)
+            {
+                return response(200, "You used POST");
+            }
+            else if (req.method == "DELETE"_method)
+            {
+                return response(200, "You used DELETE");
+            }
+            else
+            {
+                return response(404);
+            }
+        });
+
+
+
+    CROW_ROUTE(app, "/write_order_info")([&order_info](const crow::request&, crow::response& res) -> void{
+        res.add_header("Access-Control-Allow-Origin", "*");
+        start_order();
+
+        res.write("[ ");
+        
+        std :: string description = order_info;
+        std :: string name = "";
+        std :: string phone = "";
+        for (int i = 0; i < order_info.length(); i++){
+            if (order_info[i] == '_'){
+                name = order_info.substr(0, i);
+                phone = order_info.substr(i + 1, order_info.length() - i - 1);
+            }
+        }
+        cout << "\n\n\n\ndescription:" << description << "\n\n\n\n";
+        res.write(ReturnOrderInfoJson("name", name, "phone", phone, ReturnOrderInfoComand(description)));
+
+
+        res.write(" ]");
+        res.end();
+    });
+
+    
 
     app.port(18080)
         .multithreaded()
